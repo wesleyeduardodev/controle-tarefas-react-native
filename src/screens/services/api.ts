f0cae46498ff09bb89ed88adf1d8ebe351f266b7
wesleyeduardodev <wesleyeduardo.dev@gmail.com>
2025-01-11 10:23:34 -0300

    teste

--- src/screens/services/api.ts
+++ src/screens/services/api.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export const api = axios.create({
     //baseURL: 'http://192.168.1.106:8080/api',
     //baseURL: 'http://api-pedidos.us-east-1.elasticbeanstalk.com/api',
